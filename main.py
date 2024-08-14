b8932659cf371d907a1c709b79ad18b85d036d6c
128bitpotato <skickligt01.flagig@icloud.com>
2024-08-14 10:50:16 +0200

    Report updated as part of main istead of function.

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ def main():
     text = get_book_text(book_path)
     word_count = count_words(text)
     character_count = count_characters(text)
-    report = gen_report(character_count, book_path, word_count)
-    print(f"Report: {report}")
 
-def gen_report(dict, book, word):
-    char_list = [{"char": key, "count": value} for key, value in dict.items() if key.isalpha()]
+#   --- REPORT ---
+    char_list = [{"char": key, "count": value} for key, value in character_count.items() if key.isalpha()]
     def sort_on(char_list):
         return char_list["count"]
-    print(f"--- Begin report of {book} ---")
-    print(f"""{word} words found in the document
+    print(f"--- Begin report of {book_path} ---")
+    print(f"""{word_count} words found in the document
           """)
     for char in sorted(char_list, reverse=True, key=sort_on):
         print(f"The '{char['char']}' character was found {char['count']} times")
     print("""
           --- End report ---""")
-    
+
+#   --- FUNCTIONS ---    
+
 def get_book_text(path):    
     with open(path) as f:
         return f.read()
